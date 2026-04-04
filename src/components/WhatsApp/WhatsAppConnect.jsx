8ea0c0557fe285cbaecf103522b480d84d90b5a4
debashishmeher <debashishmeher955@gmail.com>
2026-04-04 11:29:26 +0530

    2

--- src/components/WhatsApp/WhatsAppConnect.jsx
+++ src/components/WhatsApp/WhatsAppConnect.jsx
@@ -39,7 +39,7 @@ export default function WhatsAppEmbeddedSignup() {
 
     const token = Cookies.get("authToken");
 
-    const res = await fetch(`${API_URL}/meta/access-token`, {
+    const res = await fetch(`${API_URL}/meta/onboard-whatsapp`, {
       method: "POST",
       credentials: "include",
       headers: {
